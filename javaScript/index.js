e426855543e2334a18d2fe9088605af61d36ef55
Michael Johnson <Michaeljcantley@gmail.com>
2023-03-26 19:07:23 -0400

    Add reset features

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,10 +1,4 @@
-/* For the intial portion of the project, log everything to the
-console. */
-
-// Will be played against the computer
-
-// May have to move the event listener over to the playRound function as it was originally
-
+// Global Variables Start
 
 const buttons = document.querySelectorAll('button');
 const playerScore = document.querySelector('.playerScore');
@@ -12,122 +6,94 @@ const computerScore = document.querySelector('.computerScore');
 let playerResult = document.querySelector('#playerScore');
 let computerResult = document.querySelector('#computerScore');
 
-console.log(playerResult, computerResult);
-
-
-let startGame = Array.from(buttons).forEach((button) => {
-    button.addEventListener('click', game);
-})
-
-
-
-
-
-// Add a div for displaying results and change all of your console.logs into DOM methods.
+const gameOver = document.querySelector('.gameOver');
 
 const div = document.createElement('div')
 div.classList.add('results');
 const selections = document.querySelector('.selection-container');
 selections.insertAdjacentElement('afterend', div);
-
-
-
-// Begin with function getComputerChoice()
-
 let choices = ['rock', 'paper', 'scissors'];
-function getComputerChoice() {
-    
-    // Randomly returns either 'Rock', 'Paper', or 'Scissors'
-    
-    // code for random number between 0 and 2 would be Math.floor(Math.random() * 3);
-    
-    return choices[Math.floor(Math.random() * 3)];
-    
-    // We will use this function in the game to make the computer's play
-    
-}
 
-// Write a function called playRound that plays a single round of Rock Paper Scissors
-
-function playRound(playerSelection, computerSelection) {
-    
-    // The function should take two parameters
-    
-    // First parameter is playerSelection
-    
-    // Second is computerSelection
-    
-    /* Return a string that declares the winner of the round
-    like so 'You lose! Paper beats Rock' */
-    
-    // Make your function's playerSelection parameter case-insensitive
-    
-    // For case insensitivity, I could toLowerCase the string before comparing
-    
-    // Return the results, not console.log
-    
-    computerSelection = getComputerChoice();
-    
-    
-    
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            
-        div.textContent = 'You win! Rock beats Scissors';
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        
-        div.textContent = 'You lose! Paper beats Rock';
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        
-        div.textContent = 'You win! Paper beats Rock';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        
-        div.textContent = 'You lose! Scissors beats Paper';
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        
-        div.textContent = 'You win! Scissors beats Paper';
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        
-        div.textContent = 'You lose! Rock beats Scissors';
-    } else {
-        
-        div.textContent = 'tie game!';
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-}
-
-// Write a new function called game()
 let myTally = 0;
 let computerTally = 0;
 let round = 0;
 
+// Global Variables End
+
+
+// Event Listener that listens for a click and starts my game function
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', game);
+})
+
+// Function meant to grab a random choice between 'Rock, Paper, or Scissors';
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+// Function that compares the player's selection to the computer's and returns a string
+
+function playRound(playerSelection, computerSelection) {
+       
+    computerSelection = getComputerChoice();
+    
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {        
+        div.textContent = 'You win! Rock beats Scissors';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        div.textContent = 'You lose! Paper beats Rock';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        div.textContent = 'You win! Paper beats Rock';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        div.textContent = 'You lose! Scissors beats Paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        div.textContent = 'You win! Scissors beats Paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        div.textContent = 'You lose! Rock beats Scissors';
+    } else {
+        div.textContent = 'tie game!';
+    }
+}
+
+// helper functions meant for resetting the game and text content
+
+function reset() {
+    myTally = 0;
+    computerTally = 0;
+    round = 0;
+    Array.from(buttons).forEach((button) => {
+        button.removeEventListener('click', game);
+    })
+}
+
+function startGame() {
+    Array.from(buttons).forEach((button) => {
+        button.addEventListener('click', game);
+    })
+}
+
+// The game function
+
 function game() {
-    
-    /* call the playRound function inside of this one to play a 5 round
-    game that keeps score and reports a winner or loser at the end. */
-    
-    // use loops to handle the rounds
-    
-    // remove the logic that plays five rounds
-    
     const roundElement = document.querySelector('.round');
     
+    // Regex's to test against the div's textContent
     let winCondition = /win/;
     let loseCondition = /lose/;
     let tieCondition = /tie/;
+
+    // When the function begins, reset the text content
+
+    if (round == 0) {
+        gameOver.textContent = '';
+    }
     
-    /* Create three buttons, one for each selection. Add an event listener
-    to the buttons that call your playRound function with the correct
-    playerSelection every time a button is clicked. (You can keep the console.log's
-    for this step) */
+    // When playRound is called, grab the event buttons' value
     
-        playRound(this.value);
+    playRound(this.value);
+
+    // Depending on the regex that matches, alter the tally counts and round
 
         if (winCondition.test(div.textContent)) {
             myTally += 1;
@@ -143,63 +109,32 @@ function game() {
             round += 1;
         }
 
+    // Update the results and round visually on the page
+
         playerResult.textContent = myTally;
         computerResult.textContent = computerTally;
         roundElement.textContent = round;
+
+    // Doesn't check until the 5th round, gives the final message
     
         if (round == 5 && myTally > computerTally) {
-            const gameOver = document.createElement('div');
-            gameOver.classList.add('gameOver');
+            
             gameOver.textContent = 'You\'re Winner!';
-            div.insertAdjacentElement('afterend', gameOver);
+            reset();
+            startGame();
         } else if (round == 5 && myTally < computerTally) {
-            const gameOver = document.createElement('div');
-            gameOver.classList.add('gameOver');
+            
             gameOver.textContent = 'You\'re Loser!';
-            div.insertAdjacentElement('afterend', gameOver);
+            reset();
+            startGame();
         } else if (round == 5 && myTally == computerTally) {
-            const gameOver = document.createElement('div');
-            gameOver.classList.add('gameOver');
+            
             gameOver.textContent = 'It\'s a tie!';
-            div.insertAdjacentElement('afterend', gameOver);
-        }
-    // for (let i = 0; i < 5; i++) {
-    //     result = playRound(prompt('enter rock, paper, or scissors'));
-
-    //     if (winCondition.test(result)) {
-    //         myScore++;
-    //         computerScore--;
-    //     } else if (loseCondition.test(result)) {
-    //         myScore--;
-    //         computerScore++;
-    //     } else if (tieCondition.test(result)) {
-    //         myScore;
-    //         computerScore;
-    //     } else {
-    //         prompt('please enter rock, paper, or scissors correctly!');
-    // }
-
-    //     console.log(`Your score is ${myScore}, the opponent's score is ${computerScore}`);
-
-    // }
-
-    
-    
+            reset();
+            startGame();
+        }     
 }
 
-// player should be able to play by clicking buttons instead of typing
 
 
 
-
-// Display the running score, and announce a winner of the game once one player reaches 5 points
-
-/* You will likely have to refactor (rework/rewrite) your original code to make it
-   work for this. That's OK! */
-
-   
-
-
-   /* When a forEach access a function via a function call, you can 
-      have access to this keyword inside of the function that the 
-      forEach has called. */
